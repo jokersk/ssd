479510bcd4c348debb6f5e35c75da70bd6e7bd02
jokersk <jokersk119@gmail.com>
2021-01-24 13:15:31 +0800

    create about us page

---
+++ pages/about-us.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+<script>
+import { client } from '@/utils/contentApi.js'
+export default {
+    mounted() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            client
+                .getEntry('4ze9WZszrnv4m6YJ4JYImZ')
+                .then((entry) => {
+                    console.log(entry)
+                })
+                .catch((err) => console.log(err))
+        },
+    },
+}
+</script>
--- utils/contentfulEntryIDs.js
+++ utils/contentfulEntryIDs.js
@@ -1,10 +1,8 @@
-const isDev = process.env.isDev
 export const entryIDs = {
-  // example entry
   homePage: {
-    hongKong: isDev ? '2tdkJfTjC0MamOO2qAWSwU' : '2tdkJfTjC0MamOO2qAWSwU'
+    hongKong: '2tdkJfTjC0MamOO2qAWSwU'
   },
   brightcoveVideo: {
-    beautyCreatorsTalkVideo: isDev ? '4qY543EXS8zltN8FyfYiVf' : '4qY543EXS8zltN8FyfYiVf'
+    beautyCreatorsTalkVideo: '4qY543EXS8zltN8FyfYiVf'
   }
 }
